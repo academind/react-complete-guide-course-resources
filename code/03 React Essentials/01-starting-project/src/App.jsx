5dcaba3e9f89edf551ee175e1e2c32009c7f579b
Joseph Searle <josephwilliamsearle@gmail.com>
2023-10-23 13:30:33 +0100

    Added dynamic content to header component

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,10 +1,16 @@
+const reactDescriptions = ['Fundermentals', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
