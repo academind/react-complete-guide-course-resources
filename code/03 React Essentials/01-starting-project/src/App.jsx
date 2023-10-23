564b5bf6ad814f48de506b8c7fad64fa28bbd61a
Joseph Searle <josephwilliamsearle@gmail.com>
2023-10-23 16:50:41 +0100

    hehe finished

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -34,10 +34,9 @@ function App() {
         <section id='core-concepts'>
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            { CORE_CONCEPTS.map((conceptItem) => {
+              return <CoreConcept key={conceptItem.title} {...conceptItem} /> 
+            })}
           </ul>
         </section>
         <section id='examples'>
