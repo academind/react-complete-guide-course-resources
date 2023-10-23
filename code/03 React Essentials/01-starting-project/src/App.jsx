c556b8cdf37c4a9d9a138bd2f654fa662adb09b0
Joseph Searle <josephwilliamsearle@gmail.com>
2023-10-23 14:03:29 +0100

    Used object destructuring in CoreConcept

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -22,12 +22,12 @@ function Header() {
   );
 }
 
-function CoreConcept(props) {
+function CoreConcept({image, title, description}) {
   return (
     <li>
-      <img src={props.image} alt='...' />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt='...' />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
