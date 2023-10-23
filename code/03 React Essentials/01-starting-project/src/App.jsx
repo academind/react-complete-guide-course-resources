137c6722f0c46277a6b8453c3f0fe7c6ba5d746d
Joseph Searle <josephwilliamsearle@gmail.com>
2023-10-23 13:31:48 +0100

    assigned dynamic content to a variable

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -5,12 +5,13 @@ function genRandomInt(max) {
 }
 
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
