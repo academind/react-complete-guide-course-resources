c51476c0af508f79d1b89b30f14bf6732643b613
Gina <gmroscioli@gmail.com>
2023-11-27 09:53:28 -0500

    separate header

---
+++ attachments/03 React Essentials/01-starting-project/src/App.jsx
@@ -0,0 +1,25 @@
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
