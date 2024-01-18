c1d68464d48d21c0da268ed4e3bd05500a254cd4
enes-dogan <98415510+enes-dogan@users.noreply.github.com>
2024-01-19 00:21:19 +0300

    added name= "email" to the input at fetcher.Form in NewsletterSignup component
    to recieving email value when newsletter action triggered.

--- code/21 Routing/30-usefetcher/src/components/NewsletterSignup.js
+++ code/21 Routing/30-usefetcher/src/components/NewsletterSignup.js
@@ -21,6 +21,7 @@ function NewsletterSignup() {
     >
       <input
         type="email"
+        name="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
