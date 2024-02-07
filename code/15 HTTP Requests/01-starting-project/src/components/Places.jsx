97f16c13099cbf9255066bac0b1d76c50b0f0045
Jannick Marcel Vieluf <jannick@vieluf.eu>
2024-02-07 14:37:22 +0100

    Fix code difference regarding to udemy course
    See Section 15, Lesson 231, Minute 03:09

--- code/15 HTTP Requests/01-starting-project/src/components/Places.jsx
+++ code/15 HTTP Requests/01-starting-project/src/components/Places.jsx
@@ -9,7 +9,7 @@ export default function Places({ title, places, fallbackText, onSelectPlace }) {
           {places.map((place) => (
             <li key={place.id} className="place-item">
               <button onClick={() => onSelectPlace(place)}>
-                <img src={`http://localhost:3000/${place.image.src}`} alt={place.image.alt} />
+                <img src={place.image.src} alt={place.image.alt} />
                 <h3>{place.title}</h3>
               </button>
             </li>
