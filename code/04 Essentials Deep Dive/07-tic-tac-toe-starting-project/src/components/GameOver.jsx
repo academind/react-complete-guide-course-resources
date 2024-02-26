5366fba1ee6b1b377a3ababbd9a35ecbae7f4a82
Robson Barbosa <116354613+robson-barbosa@users.noreply.github.com>
2024-02-26 15:32:40 -0300

    Jogo da Velha

---
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameOver.jsx
@@ -0,0 +1,12 @@
+export default function GameOver({ winner, onRestart }) {
+  return (
+    <div id="game-over">
+      <h2>Game Over!</h2>
+      {winner && <p>{winner} won!</p>}
+      {!winner && <p>It's a draw!</p>}
+      <p>
+        <button onClick={onRestart}>Rematch!</button>
+      </p>
+    </div>
+  );
+}
