9a8caeb313fa8447684f95c60cf4ed3135a334a1
Kevin Andres <khernandez@growthaccelerationpartners.com>
2024-03-05 08:56:22 -0600

    Add header and player component

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,9 +1,16 @@
+import Player from "./components/Player"
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="PLayer 2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
   )
 }
 
