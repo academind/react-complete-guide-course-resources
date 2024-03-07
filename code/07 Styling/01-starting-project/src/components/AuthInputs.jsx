d90dabab22477b2a62736fa839b59c219921afff
Kevin Andres <khernandez@growthaccelerationpartners.com>
2024-03-07 10:46:28 -0600

    Add styled-components and Header.module.css for styling

--- code/07 Styling/01-starting-project/src/components/AuthInputs.jsx
+++ code/07 Styling/01-starting-project/src/components/AuthInputs.jsx
@@ -1,4 +1,33 @@
 import { useState } from 'react';
+import { styled } from 'styled-components';
+
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: #6b7280;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: #d1d5db;
+  color: #374151;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -22,18 +51,22 @@ export default function AuthInputs() {
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
+      <ControlContainer>
         <p>
-          <label>Email</label>
-          <input
+          {/* Add ccs class conditionally based on emailNotValid */}
+          <Label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</Label>
+          <Input
             type="email"
+            // style={{
+            //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db',
+            // }}
             className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
-          <input
+          <Label className={`label ${passwordNotValid ? 'invalid' : ''}`}>Password</Label>
+          <Input
             type="password"
             className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
@@ -41,7 +74,7 @@ export default function AuthInputs() {
             }
           />
         </p>
-      </div>
+      </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
--- code/07 Styling/01-starting-project/src/components/Header.jsx
+++ code/07 Styling/01-starting-project/src/components/Header.jsx
@@ -1,11 +1,20 @@
 import logo from '../assets/logo.png';
+import classes from './Header.module.css';
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      {/* <p style={{
+        color: 'red',
+        fontSize: '1.25rem',
+        fontWeight: '300',
+        textAlign: 'center',
+      
+      }}>A community of artists and art-lovers.</p> */}
+
+      <p className={classes.paragraph}>A community of artists and art-lovers.</p>
     </header>
   );
 }
