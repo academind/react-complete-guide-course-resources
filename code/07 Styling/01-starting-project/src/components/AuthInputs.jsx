ac2ec45c3d3ca0bb9fa434119721e52868c4a0ca
Kevin Andres <khernandez@growthaccelerationpartners.com>
2024-03-07 12:07:47 -0600

    Add Button and Input components for authentication inputs

--- code/07 Styling/01-starting-project/src/components/AuthInputs.jsx
+++ code/07 Styling/01-starting-project/src/components/AuthInputs.jsx
@@ -1,47 +1,13 @@
 import { useState } from 'react';
 import { styled } from 'styled-components';
+import Button from './Button';
+import Input from './Input';
 
 const ControlContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`;
-
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({$invalid}) => $invalid ? '#f87171' : '#6b7280'};
-`;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({$invalid}) => $invalid ? '#fed2d2' : '#d1d5db'};
-  color: ${({$invalid}) => $invalid ? '#ef4444' : '#374151'};
-  border: 1px solid ${({$invalid}) => $invalid ? '#f73f3f' : 'transparent'};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
-
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-
-  &:hover {
-    background-color: #f0920e;
-  }
 `;
 
 export default function AuthInputs() {
@@ -67,31 +33,25 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <ControlContainer>
-        <p>
-          {/* Add ccs class conditionally based on emailNotValid */}
-          <Label $invalid={emailNotValid}>Email</Label>
-          <Input
-            type="email"
-            // style={{
-            //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db',
-            // }}
-            //className={emailNotValid ? 'invalid' : undefined}
-            $invalid={emailNotValid}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p>
-        <p>
-          {/* <Label className={`label ${passwordNotValid ? 'invalid' : ''}`}>Password</Label> */}
-          <Label $invalid={passwordNotValid}>Password</Label>
-          <Input
-            type="password"
-            $invalid={passwordNotValid}
-            // className={passwordNotValid ? 'invalid' : undefined}
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-          />
-        </p>
+
+        <Input
+          label="Email"
+          invalid={emailNotValid}
+          type="email"
+          $invalid={emailNotValid}
+          onChange={(event) => handleInputChange('email', event.target.value)}
+        />
+
+        <Input
+          label="Password"
+          invalid={passwordNotValid}
+          type="password"
+          $invalid={passwordNotValid}
+          onChange={(event) =>
+            handleInputChange('password', event.target.value)
+          }
+        />
+
       </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
---
+++ code/07 Styling/01-starting-project/src/components/Button.jsx
@@ -0,0 +1,18 @@
+import { styled } from 'styled-components';
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+    background-color: #f0920e;
+  }
+`;
+
+export default Button;
