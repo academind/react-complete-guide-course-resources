45699b64bf0615c86bc2b31ca3eea243b16072a4
ahmell <andrewmell10@gmail.com>
2024-04-26 17:28:43 -0500

    Worked on passing props through different components to dynamically change them, while using the same component tag.

--- attachments/03 React Essentials/01-starting-project/src/App.jsx
+++ attachments/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,10 +1,22 @@
+import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data.js"
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description =
+    reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
+  //Keeping javascript code outside of your JSX returns is generally considered good practice.
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
@@ -12,13 +24,38 @@ function Header() {
 }
 
 //you execute functions/components not by calling them, but with instanciating as if they were HTML elements.
+//React components MUST start with an uppercase character, to tell React that it's not a built-in component, like <header> or <Header>
+
+function CoreConcept({image, title, description}) {
+  //you can destructure incoming data to write less!
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
 
 function App() {
   return (
     <div>
-      <Header />  {/* Components can have an opening and closing tag, or can be self-closing, as you see here*/}
-      <main>  
-        <h2>Time to get started!</h2>
+      <Header />{" "}
+      {/* Components can have an opening and closing tag, or can be self-closing, as you see here*/}
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
