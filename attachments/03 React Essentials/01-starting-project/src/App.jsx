4ca7ca6915c2fb90196082a1e7bd14115ffa6f1c
ahmell <andrewmell10@gmail.com>
2024-05-01 19:17:45 -0500

    began working on state, and displaying content using conditional statements.

--- attachments/03 React Essentials/01-starting-project/src/App.jsx
+++ attachments/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,18 +1,39 @@
+import { useState } from "react";
+
+// useState is a React hook, which is technically a regular function, but can only be called inside react functions or other react hooks
 import { CORE_CONCEPTS } from "./data.js";
-import { Header }  from "./components/Header/Header.jsx";
+import { Header } from "./components/Header/Header.jsx";
 import { CoreConcept } from "./components/CoreConcept/CoreConcept.jsx";
 import TabButton from "./components/TabButton/TabButton.jsx";
-
-function handleClick () {
-  console.log("hello world! - Clicked!")
-}
+import { EXAMPLES } from "./data.js";
 
 //you execute functions/components not by calling them, but with instanciating as if they were HTML elements.
 //React components MUST start with an uppercase character, to tell React that it's not a built-in component, like <header> or <Header>
 
-
-
+//useState can only be called at the top level of a component function.
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleClick(selectedButton) {
+    setSelectedTopic(selectedButton);
+    //console.log(selectedButton) The previous state will be logged upon updating, however, the UI will be correct in its display.
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
+  // state is a way of telling React that we want to update the UI
   return (
     <div>
       <Header />{" "}
@@ -21,9 +42,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]}/>
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept
               title={CORE_CONCEPTS[3].title}
               description={CORE_CONCEPTS[3].description}
@@ -34,12 +55,15 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onClick={handleClick}>Components</TabButton>
-            <TabButton onClick={handleClick}>JSX</TabButton>
-            <TabButton onClick={handleClick}>Props</TabButton>
-            <TabButton onClick={handleClick}>State</TabButton>
+            <TabButton onClick={() => handleClick("components")}>
+              Components
+            </TabButton>
+            {/* The anonymous arrow function allows a value to be passed to the function on the right */}
+            <TabButton onClick={() => handleClick("jsx")}>JSX</TabButton>
+            <TabButton onClick={() => handleClick("props")}>Props</TabButton>
+            <TabButton onClick={() => handleClick("state")}>State</TabButton>
           </menu>
-
+          {tabContent}
         </section>
       </main>
     </div>
