404bcff784c501f8d97b2c3f2c842ce064448199
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2024-05-22 11:23:45 +0200

    fixed resource

--- code/18 Practice Project - Food Order/12-finishing-touches/backend/app.js
+++ code/18 Practice Project - Food Order/12-finishing-touches/backend/app.js
@@ -23,7 +23,7 @@ app.get('/meals', async (req, res) => {
 app.post('/orders', async (req, res) => {
   const orderData = req.body.order;
 
-  if (orderData === null || orderData.items === null || orderData.items === []) {
+  if (orderData === null || orderData.items === null || orderData.items.length === 0) {
     return res
       .status(400)
       .json({ message: 'Missing data.' });
