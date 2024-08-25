1531dedeaa6ad579106b795517e3ed277f7939e9
Fred <huertaserick@yahoo.ie>
2024-08-25 16:55:12 +0100

    Training

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,63 +1,32 @@
-import React, { useState } from "react";
+import reactImg from "./assets/react-core-concepts.png";
+
+const reactDescription = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
 
 function Header() {
+  const description = reactDescription[genRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
   );
 }
 
-function MainGoal({ id }) {
-  return (
-    <p id={id}>
-      My main goal is to learn react, from the ground up and in depth!!
-    </p>
-  );
-}
-
-function AskName() {
-  const [name, setName] = useState("");
-  function handleClick() {
-    const inputName = prompt("What is your name ?");
-    setName(inputName);
-  }
-
-  return (
-    <div>
-      <button onClick={handleClick}>Tell me your name</button>
-      {name && <p>Hello there !{name}</p>}
-    </div>
-  );
-}
-
-function Button({ onClick }) {
-  return (
-    <button onClick={onClick}>
-      <label>This is a button.</label>
-    </button>
-  );
-}
-
 function App() {
-  function handleButtonClick() {
-    alert("You pressed the button!");
-  }
-
   return (
     <div>
       <Header />
       <main>
         <h2>Time to get started!</h2>
-        <AskName />
       </main>
-      <MainGoal id="maingoal"></MainGoal>
-      <Button onClick={handleButtonClick}></Button>
     </div>
   );
 }
