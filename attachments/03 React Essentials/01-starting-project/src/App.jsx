433c8e7f003824f9649a8dbfc2cc9e6a7256095c
09372052 <ebru.ustuner@ext.kocdigital.com>
2025-02-18 15:37:59 +0300

    feat: dynamic content add

--- attachments/03 React Essentials/01-starting-project/src/App.jsx
+++ attachments/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,10 +1,21 @@
+import reactImg from './assets/react-core-concepts.png'
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function getRandomIn(max){
+  return Math.floor(Math.random() * (max + 1));
+}
+
+
 function Header() {
+  const description = reactDescriptions[getRandomIn(2)];
+
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials - header</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
